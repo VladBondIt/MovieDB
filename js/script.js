a12580871c4aa1e114950864f9b54208995be9e3
Vlad Bond <foolowin@gmail.com>
2020-11-02 20:39:09 +0300

    tbc

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ const form = document.querySelector('.add'),
     promoGenre = document.querySelector('.promo__genre'),
     promoBg = document.querySelector('.promo__bg'),
     titleAds = document.querySelector('.promo__adv-title');
-
-let movieLists = document.querySelectorAll('.promo__interactive-item');
+let triggerCarts = document.querySelectorAll('.delete'),
+    movieLists = document.querySelectorAll('.promo__interactive-item');
 
 titleAds.remove();
 ads.forEach(item => {
@@ -33,37 +33,15 @@ promoBg.style.background = `url('../img/bg.jpg') center / 100% 100% no-repeat`;
 
 
 
-function movieSort(lists) {
-    lists = document.querySelectorAll('.promo__interactive-item');
-    movieDB.movies.sort();
-    lists.forEach((list, j) => {
-        movieDB.movies.forEach((item, i) => {
-            if (i == j) {
-                list.innerHTML = `№ ${j + 1} ${item}`;
-            }
-        });
-        let cart = document.createElement('div');
-        cart.classList.add('delete');
-        list.append(cart);
 
-    });
-}
 
-movieSort(movieLists);
-
-// Form add
+// Form add movie
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let addMovie = input.value;
     if (addMovie != '...' && addMovie != ' ' && addMovie != '' && addMovie.length <= 21) {
-        let addNewMovie = document.createElement('li');
-        addNewMovie.innerHTML = addMovie;
-        addNewMovie.classList.add('promo__interactive-item');
         movieDB.movies.push(addMovie);
-        movieList.append(addNewMovie);
-        movieSort(movieLists);
-        triggerCarts = document.querySelectorAll('.delete');
         setTimeout(() => {
             input.value = '';
         }, 200);
@@ -78,34 +56,8 @@ form.addEventListener('submit', (e) => {
 
 // delete film
 
-let triggerCarts = document.querySelectorAll('.delete');
-console.log(triggerCarts);
-function deleteMovie() {
 
-    triggerCarts.forEach((item, i) => {
-        item.addEventListener('click', (e) => {
-            item.parentNode.remove();
-            item.remove();
-            movieDB.movies.forEach((movieInDb, j) => {
-                if (i == j) {
-                    movieDB.movies.splice(i, 1);
-                }
-            });
 
-            console.log(movieDB.movies);
-            triggerCarts = document.querySelectorAll('.promo__interactive-item div');
-            movieLists = document.querySelectorAll('.promo__interactive-item');
-            movieSort(movieLists);
-            console.log(movieLists);
-            triggerCarts = document.querySelectorAll('.promo__interactive-item div');
-            console.log(triggerCarts);
-        });
-
-    });
-
-}
-
-deleteMovie();
 
 
 function chacked() {
